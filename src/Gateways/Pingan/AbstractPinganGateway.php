f7b7ac7a40363f4ea32bfd3593f771ea1e296338
wangjian <1636801376@qq.com>
2018-06-21 11:55:17 +0800

    支付回调功能开发

--- src/Gateways/Pingan/AbstractPinganGateway.php
+++ src/Gateways/Pingan/AbstractPinganGateway.php
@@ -63,6 +63,83 @@ abstract class AbstractPinganGateway extends AbstractGateway
         return $this->doCharge($response, $form);
     }
 
+    public function refund(Refund $form): array
+    {
+        $options = [
+            'sign_type' => $this->config->get('sign_type', 'RSA'),
+            'out_no' => $form->get('order_id'),
+            'refund_out_no' => $form->get('refund_id'),
+            'refund_amount' => $form->get('refund_amount'),
+            'shop_pass' => $this->config->get('shop_pass', null),
+            'trade_account' => $this->config->get('trade_account', null),
+        ];
+
+        if(!empty($form->get('trade_no'))) {
+            $options['trade_no'] = $form->get('trade_no');
+        }
+
+        return $this->client->refund($options);
+    }
+
+    public function close(Close $form): array
+    {
+        // TODO: Implement close() method.
+    }
+
+    public function query(Query $form): array
+    {
+        // TODO: Implement query() method.
+    }
+
+    public function chargeNotify(array $receives): array
+    {
+        $tradeResult = json_decode($receives['trade_result'], true);
+        return [
+            'order_id' => $receives['out_no'],
+            'status' => 'paid', // 微信只推送支付完成
+            'trade_sn' => $receives['ord_no'],
+            'buyer_identifiable_id' => $tradeResult['openid'],
+            'buyer_is_subscribed' => 'N' === $tradeResult['is_subscribe'] ? 'no' : 'yes',
+            'amount' => $receives['amount'],
+            'buyer_name' => '',
+            'paid_at' => $this->normalizePayTime($receives['pay_time']),
+            'raw' => $receives,
+        ];
+    }
+
+    public function refundNotify(array $receives): array
+    {
+    }
+
+    public function closeNotify(array $receives): array
+    {
+    }
+
+    public function verify($receives): bool
+    {
+        return $this->client->verifyResponse($receives);
+    }
+
+    public function success(): string
+    {
+        return $this->client->success();
+    }
+
+    public function fail(): string
+    {
+        return $this->client->failed();
+    }
+
+    public function receiveNotificationFromRequest()
+    {
+        return $_POST;
+    }
+
+    public function convertNotificationToArray($receives): array
+    {
+        return $receives;
+    }
+
     /**
      * @param Charge $form
      *
@@ -82,4 +159,10 @@ abstract class AbstractPinganGateway extends AbstractGateway
      * @return string
      */
     abstract protected function getTradeType(): string;
+
+    protected function normalizePayTime($payTime)
+    {
+        preg_match('/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/', $payTime, $matches);
+        return "$matches[1]-$matches[2]-$matches[3] $matches[4]:$matches[5]:$matches[6]";
+    }
 }
